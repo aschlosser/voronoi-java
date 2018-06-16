ebd8f7f9ddb423771f2a98b76c454b45ae1b160b
nmasondo <39006900+nmasondo@users.noreply.github.com>
2018-06-16 15:20:46 +0200

    Update VoronoiTest.java

--- src/test/java/de/alsclo/voronoi/VoronoiTest.java
+++ src/test/java/de/alsclo/voronoi/VoronoiTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
